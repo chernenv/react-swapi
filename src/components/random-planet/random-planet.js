b435789d3ac3d0db7b45fa03ae8f0f8fdf38278c
chernenv <psycho2868@gmail.com>
2020-08-01 16:31:07 +0200

    spinner + error indicator

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import SwapiService from "../../services/swapi-service";
+
+import Spinner from '../spinner';
+import ErrorIndicator from "../error-indicator";
+import SwapiService from '../../services/swapi-service';
+
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+
     swapiService = new SwapiService();
 
     state = {
-        planet: {}
+        planet: {},
+        loading: true,
+        error: false
     };
 
     constructor() {
@@ -15,42 +22,75 @@ export default class RandomPlanet extends Component {
     }
 
     onPlanetLoaded = (planet) => {
-        this.setState({planet})
-    }
+        this.setState({
+            planet,
+            loading: false
+        });
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    };
 
     updatePlanet() {
-        const id = Math.floor(Math.random() * 25) + 2;
-        this.swapiService.getPlanet(id)
+        const id = 123123;
+        this.swapiService
+            .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
 
     render() {
-
-        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state
+        const {planet, loading, error} = this.state;
+        const hasData = !(loading || error);
+        const errorMessage = error ? <ErrorIndicator/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = hasData ? <PlanetView planet={planet}/> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
-                <div>
-                    <h4>{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
-
         );
     }
 }
+
+const PlanetView = ({planet}) => {
+
+    const {
+        id, name, population,
+        rotationPeriod, diameter
+    } = planet;
+
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    );
+};
+
+
+
